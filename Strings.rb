bb72e114c7ea7e30df94fbe509f730bb28e281e0
beachcoder25 <cornish25@gmail.com>
2019-03-30 00:52:16 -0700

    Strings and String Methods

---
+++ Strings.rb
@@ -0,0 +1,38 @@
+# .length method
+# Must use puts to output to console
+
+puts "Length of this String".length
+
+# O:
+# 21
+
+
+# .reverse method
+# Returns the reverse of the string provided
+
+puts "Backward".reverse
+puts "drawroF".reverse
+
+# O:
+# drawkcaB
+# Forward
+
+
+# .upcase & .downcase
+
+puts "Upper".upcase
+puts "Lower".downcase
+
+# O:
+# UPPER
+# lower
+
+
+# Multi-Line Comments
+# = MUST be adjacent to begin/end
+# begin/end must be on liens by themselves
+
+=begin
+This is more than
+One line
+=end
