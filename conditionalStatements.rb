d98b9947a516583528bf97d77981d84b3db23765
beachcoder25 <cornish25@gmail.com>
2019-04-03 14:34:41 -0700

    if, elsif, else

---
+++ conditionalStatements.rb
@@ -0,0 +1,10 @@
+# Put end after last conditional statement, not after each
+
+if 11 != 0
+    puts "Eleven must not be equal to 0"
+  elsif 11 == 0
+    puts "Eleven must equal 0 (somehow)"
+  else
+    puts "Eleven neither equals 0, nor does not equal 0"
+  end
+      
